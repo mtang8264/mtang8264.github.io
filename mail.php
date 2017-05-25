c20a53d61a97fcf82c13e075581ef4219d432e95
Michael Tang <mtang8264@gmail.com>
2017-05-25 13:05:14 -0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -39,3 +39,111 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 }
 	}
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Contact - WE Advocate Mental Health</title>
+		
+		<link rel="stylesheet"
+			type="text/css"
+			href="myStyle.css"/>
+	</head>
+
+	<body>
+		<!-- logo and navigation -->
+		<a href="index.html"><img src="assets/logo.jpg" alt="Logo" id="logo"></a>
+		<br>
+		<table style="display: block; width: 100%; overflow-x: auto;">
+			<td id="navitem">
+				<a href="index.html"><h3>Home</h3></a>
+			</td>
+			<td id="navitem">
+				<a href="resources.html"><h3>Resources</h3></a>
+			</td>
+			<td id="navitem">
+				<a href="team.html"><h3>Our Team</h3></a>
+			</td>
+			<td id="navitem">
+				<a href="blog.html"><h3>Team Blog</h3></a>
+			</td>
+			<td id="navitem">
+				<a href="contact.html"><h3>Contact</h3></a>
+			</td>
+		</table>
+		
+		<!-- contents -->
+		<p id="spacebar"></p>
+    
+      		<div style="padding-left: 20px">
+			<form class="form-horizontal" role="form" method="post" action="index.php">
+	<div class="form-group">
+		<label for="name" class="col-sm-2 control-label">Name</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+			<?php echo "<p class='text-danger'>$errName</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="email" class="col-sm-2 control-label">Email</label>
+		<div class="col-sm-10">
+			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+			<?php echo "<p class='text-danger'>$errEmail</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="message" class="col-sm-2 control-label">Message</label>
+		<div class="col-sm-10">
+			<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+			<?php echo "<p class='text-danger'>$errMessage</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+			<?php echo "<p class='text-danger'>$errHuman</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<?php echo $result; ?>	
+		</div>
+	</div>
+</form>
+		</div>
+		<br>
+		<br>
+		
+		<!-- footer -->
+		<div style="background-color: #00a9e7; height: 125px; max-width: 200% !important; position: relative;">
+			<div id="footy" style="max-width: 200% !important">
+				<p id="foot">
+					&copy;2017 Michael Tang | Philadelphia, PA
+				</p>
+				<p id="foot">
+					<a href="index.html">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;
+					|&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="resources.html">Resources</a>&nbsp;&nbsp;&nbsp;&nbsp;
+					|&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="team.html">Our Team</a>&nbsp;&nbsp;&nbsp;&nbsp;
+					|&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="blog.html">Team Blog</a>&nbsp;&nbsp;&nbsp;&nbsp;
+					|&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="log.html">Full Activity Log</a>
+				</p>
+				<p id="foot">
+					<a href="contact.html">Contact</a>
+				</p>
+				<br>
+				<br>
+				<br>
+			</div>
+		</div>
+  </body>
+</html>
